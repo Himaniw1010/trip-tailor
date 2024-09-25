0de1e71693039c33051f33248b9386fcaec1122d
Himani Wadhera <A00304535@canadahanson.ca>
2024-09-25 10:12:56 -0400

    Cleaned Default Design

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+import styles from './Footer.module.css';
+ 
+function Footer(props) {
+  return (
+    <div className={styles.wrapper}>
+     Footer
+    </div>
+  );
+}
+
+export default Footer;
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,11 @@
+import styles from './Header.module.css';
+ 
+function Header(props) {
+  return (
+    <div className={styles.wrapper}>
+     Header
+    </div>
+  );
+}
+
+export default Header;
